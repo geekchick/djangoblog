a07d1099157e35fe3a567d83216d622919fd3a50
Tonya Sims <tsims25@gmail>
2018-12-10 17:39:01 -0600

    Checking in Django blog project

---
+++ tonyasblog/urls.py
@@ -0,0 +1,30 @@
+"""tonyasblog URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, re_path
+#from posts import views #import views files
+import posts.views
+import sitepages.views
+from django.conf.urls.static import static # import image, allows us to serve images
+from django.conf import settings # import image, get info inside settings.py
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', posts.views.home, name="home"),
+    re_path('posts/(?P<post_id>[0-9]+)/$', posts.views.post_details, name = "post_detail"),
+    path('about/', sitepages.views.about, name="about"),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
